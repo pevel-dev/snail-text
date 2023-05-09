bedeb0d22276204056a41c6c654b020b4b9309f4
pevel-dev <kkollop123@gmail.com>
2023-05-09 18:15:56 +0500

    fixiki

--- crdt/Treedoc.py
+++ crdt/Treedoc.py
@@ -2,7 +2,7 @@ class Treedoc:
     def __init__(self):
         self.root = None
 
-    def insert(self, pos_id_left, pos_id_right, local_tick, client_id):
+    def insert(self, value, pos_id_left, pos_id_right, local_tick, client_id):
         if pos_id_left is None and pos_id_right is None:
             pass
         elif pos_id_left is None and pos_id_right is not None:
@@ -15,10 +15,10 @@ class Treedoc:
     def delete(self, pos_id):
         pass
 
-    def insert_at(self, index, local_tick, client_id):
+    def insert_at(self, value, index, local_tick, client_id):
         pass
 
-    def delete_at(self, pos_id):
+    def delete_at(self, index):
         pass
 
     def get_sequence(self):

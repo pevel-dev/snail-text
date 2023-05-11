2e965a734988299b8d04a169b4769f35d371442c
pevel-dev <kkollop123@gmail.com>
2023-05-11 15:49:54 +0500

    remove trash

--- tests/Treedoc/test_crdt/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from crdt.Treedoc import Treedoc
-
-
-@pytest.fixture
-def treedoc():
-    s = Treedoc(0, 0)
-    return s
--- tests/Treedoc/test_crdt/implementaion_treedoc_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from crdt.Treedoc import Treedoc
-
-
-def test_parent_or_grandparent():
-    assert Treedoc.parent_or_grandparent("00", "0001")
-    assert Treedoc.parent_or_grandparent("1", "10101")
-    assert not Treedoc.parent_or_grandparent("000", "00100")
